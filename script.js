28b061af8414c56844e1c5cb64d273b802096776
Mayorie Technologies <mayoriewebdev@gmail.com>
2026-03-08 16:36:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
+
+hamburger.addEventListener("click", function(){
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll(".nav-menu a").forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.remove("active");
+        hamburger.classList.remove("active");
+    });
+});
